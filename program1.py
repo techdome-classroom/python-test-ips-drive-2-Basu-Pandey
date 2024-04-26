257eb2b29364ac9aed97ab9ea412311df351a1b3
Basu Pandey <basupandey1234@gmail.com>
2024-04-26 16:03:02 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@ class Solution(object):
   
     def isValid(self, s):
         stack = [] 
-        for c in s: # loop through each character in the string
+        for c in s:
             if c in '([{': 
                 stack.append(c) # push it onto the stack
             else:
